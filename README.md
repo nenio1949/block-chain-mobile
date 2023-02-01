03ac8f9bf5296cb273a8d3549fbbb79d23ad9413
yong.li <yong.li@funenc.com>
2023-02-01 20:09:51 +0800

    结构调整

--- README.md
+++
@@ -1,8 +0,0 @@
-# Vue 3 + TypeScript + Vite + Vant
-
-## 初始化
-yarn
-yarn dev
-## 打包
-yarn build
-运行打包的代码 serve -s dist 
---
+++ assets/use-id-8c97c9e1.js
@@ -0,0 +1,1 @@
+import{K as e}from"./index-9ed142a6.js";let r=0;function u(){const n=e(),{name:t="unknown"}=(n==null?void 0:n.type)||{};return`${t}-${++r}`}export{u};
