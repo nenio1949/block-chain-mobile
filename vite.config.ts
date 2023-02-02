c1d7541f9cc54e8cdd7c340b7088c65a081903cc
yong.li <yong.li@funenc.com>
2023-02-02 10:18:41 +0800

    fix: 修改vite打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { resolve } from "path";
 // import Components from "unplugin-vue-components/vite";
 // import { VantResolver } from "unplugin-vue-components/resolvers";
 
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -18,6 +21,19 @@ export default defineConfig({
       "@": resolve(__dirname, "src") // 路径别名
     },
     extensions: [".js", ".json", ".ts"] // 使用路径别名时想要省略的后缀名，可以自己 增减
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+        sanitizeFileName(name) {
+          const match = DRIVE_LETTER_REGEX.exec(name);
+          const driveLetter = match ? match[0] : "";
+          // substr 是被淘汰語法，因此要改 slice
+          return driveLetter + name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "");
+        }
+      }
+    }
   },
   server: {
     host: "0.0.0.0"
