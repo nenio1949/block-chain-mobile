eb8ec09bb2044ea1801aac894478af5b2fa4029f
yong.li <yong.li@funenc.com>
2023-02-02 10:28:08 +0800

    fix: 修改路由base

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -25,7 +25,7 @@ const routes = [
 ];
 // 1.返回一个 router 实列，为函数，里面有配置项（对象） history
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory("/block-chain-mobile/"),
   routes
 });
 
